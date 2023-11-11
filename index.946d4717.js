0d33e03d2f4fa45b4deb9a70d95c1861f2a36f53
Cotterman-b <119392287+Cotterman-b@users.noreply.github.com>
2023-11-11 11:52:33 +0900

    Add files via upload

---
+++ index.946d4717.js
@@ -0,0 +1,3 @@
+let e={root:document.querySelector(".white-background"),rootMargin:"0px",threshold:1};new IntersectionObserver(callback,e),document.querySelector(".navbar"),navObserver.observe();//# sourceMappingURL=index.946d4717.js.map
+
+//# sourceMappingURL=index.946d4717.js.map
