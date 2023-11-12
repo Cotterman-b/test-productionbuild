b94986c117a792a771ea9969543781bd99df7fe3
Cotterman-b <119392287+Cotterman-b@users.noreply.github.com>
2023-11-11 21:05:28 -0600

    Delete index.946d4717.js

--- index.946d4717.js
+++
@@ -1,3 +0,0 @@
-let e={root:document.querySelector(".white-background"),rootMargin:"0px",threshold:1};new IntersectionObserver(callback,e),document.querySelector(".navbar"),navObserver.observe();//# sourceMappingURL=index.946d4717.js.map
-
-//# sourceMappingURL=index.946d4717.js.map
