ae58b58d58e5bad1c732d2a5afa26519a72083f3
Cotterman-b <119392287+Cotterman-b@users.noreply.github.com>
2023-11-11 21:31:48 -0600

    Delete index.3dc7451b.js

--- index.3dc7451b.js
+++
@@ -1,3 +0,0 @@
-const e=()=>{let e=document.querySelectorAll(".stats"),t=new IntersectionObserver((e,t)=>{e.forEach(e=>{e.isIntersecting&&(e.target.classList.add("movein"),t.unobserve(e.target))})});e.forEach(e=>{t.observe(e)})};document.addEventListener("DOMContentLoaded",()=>{e()});//# sourceMappingURL=index.3dc7451b.js.map
-
-//# sourceMappingURL=index.3dc7451b.js.map
